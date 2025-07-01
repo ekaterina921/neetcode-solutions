657d0431cdf087771774fc5379289e2338a112fd
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-01 14:50:22 -0500

    Add calls for all solutions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import solutions.DuplicatesInArrays;
+import solutions.TwoSumArrays;
 import solutions.ValidAnagramArrays;
+
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println((new DuplicatesInArrays()).hasDuplicate(new int[]{23, 23, 45}));
         System.out.println(ValidAnagramArrays.isAnagram("ccbc", "bbcc"));
+        System.out.println(Arrays.toString(TwoSumArrays.twoSum(new int[]{2, 3, 4}, 5)));
     }
 }
