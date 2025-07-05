c554baed8e2921b744654e0a5f2c25ca52adda44
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-05 13:13:19 -0500

    Add ValidParentheses.java

---
+++ src/main/java/solutions/stack/ValidParentheses.java
@@ -0,0 +1,29 @@
+package solutions.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValidParentheses(String s){
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()){
+            if(closeToOpen.containsKey(c)){
+                if(!stack.isEmpty() && stack.peek() == closeToOpen.get(c)){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+                }
+            else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
