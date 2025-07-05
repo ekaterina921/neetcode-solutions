7bae8683b2c8d4a2f9f62ed403096f00afc862b2
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-05 12:18:40 -0500

    Fix formatting in BestTimeToBuyAndSellStock.java

--- src/main/java/solutions/slidingWindow/BestTimeToBuyAndSellStock.java
+++ src/main/java/solutions/slidingWindow/BestTimeToBuyAndSellStock.java
@@ -1,21 +1,18 @@
 package solutions.slidingWindow;
 
 public class BestTimeToBuyAndSellStock {
-
     public static int findBestTimeToBuyAndSellStock(int[] prices) {
         int leftPointer = 0, rightPointer = 1;
         int maxProfit = 0;
-        while(rightPointer < prices.length){
-            if(prices[leftPointer] < prices[rightPointer]) {
+        while (rightPointer < prices.length) {
+            if (prices[leftPointer] < prices[rightPointer]) {
                 int profit = prices[rightPointer] - prices[leftPointer];
                 maxProfit = Math.max(maxProfit, profit);
+            } else {
+                leftPointer = rightPointer;
             }
-                else {
-                    leftPointer = rightPointer;
-                }
-                rightPointer++;
-            }
+            rightPointer++;
+        }
         return maxProfit;
     }
-
 }
