04bb1d98dadd9fd54f4fd83aef1a36610a003240
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-07 09:19:31 -0500

    Update ValidParentheses.java

--- src/main/java/solutions/stack/ValidParentheses.java
+++ src/main/java/solutions/stack/ValidParentheses.java
@@ -5,22 +5,21 @@ import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses {
-    public static boolean isValidParentheses(String s){
+    public static boolean isValidParentheses(String s) {
         Stack<Character> stack = new Stack<>();
-        Map<Character,Character> closeToOpen = new HashMap<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
         closeToOpen.put(')', '(');
         closeToOpen.put(']', '[');
         closeToOpen.put('}', '{');
 
-        for (char c : s.toCharArray()){
-            if(closeToOpen.containsKey(c)){
-                if(!stack.isEmpty() && stack.peek() == closeToOpen.get(c)){
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
                     stack.pop();
                 } else {
                     return false;
                 }
-                }
-            else {
+            } else {
                 stack.push(c);
             }
         }
