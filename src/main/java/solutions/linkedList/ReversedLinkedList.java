1f759e36cf5852b66dc8725a56d6f385e17da3d5
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-10 22:07:43 -0500

    Add ReversedLinkedList.java

--- src/main/java/solutions/linkedList/ReversedLinkedList.java
+++ src/main/java/solutions/linkedList/ReversedLinkedList.java
@@ -1,4 +1,22 @@
 package solutions.linkedList;
 
 public class ReversedLinkedList {
+    int val;
+    ReversedLinkedList next;
+    ReversedLinkedList(){}
+    ReversedLinkedList(int val){this.val = val;}
+    ReversedLinkedList(int val, ReversedLinkedList next){this.val = val; this.next = next;}
+
+    public ReversedLinkedList reverseLinkedList(ReversedLinkedList head) {
+        ReversedLinkedList previous = null;
+        ReversedLinkedList current = head;
+
+        while (current != null) {
+            ReversedLinkedList next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
 }
