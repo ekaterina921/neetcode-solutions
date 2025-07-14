8ff16d63c7530bc0f89f49923e5795bbde8a9293
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-14 14:28:30 -0500

    Add ReversedLinkedListTest.java

--- src/main/java/solutions/linkedList/ReversedLinkedListTest.java
+++ src/main/java/solutions/linkedList/ReversedLinkedListTest.java
@@ -3,7 +3,7 @@ package solutions.linkedList;
 public class ReversedLinkedListTest {
     int val;
     ReversedLinkedListTest listNode;
-    public ReversedLinkedListTest() {};
+    public ReversedLinkedListTest() {}
     public ReversedLinkedListTest(int val){this.val = val;}
     public ReversedLinkedListTest(ReversedLinkedListTest listNode, int val){
         this.listNode = listNode;
@@ -20,5 +20,37 @@ public class ReversedLinkedListTest {
             current = next;
         }
         return head;
+    }
+
+    public static ReversedLinkedListTest mergeTwoSortedLists(ReversedLinkedListTest head1, ReversedLinkedListTest head2){
+        ReversedLinkedListTest dummy = new ReversedLinkedListTest(0);
+        ReversedLinkedListTest reversedLinkedListTest = dummy;
+        while (head1 != null && head2 != null){
+            if (head1.val <= head2.val) {
+                reversedLinkedListTest.listNode = head1;
+                head1 = head1.listNode;
+            } else {
+                reversedLinkedListTest.listNode = head2;
+                head2 = head2.listNode;
+            }
+            reversedLinkedListTest = reversedLinkedListTest.listNode;
+        }
+        if (head1 != null){
+            reversedLinkedListTest.listNode = head1;
+        } else if (head2 != null) {
+            reversedLinkedListTest.listNode = head2;
+        }
+        return dummy.listNode;
+    }
+
+    public static boolean detectCycle(ReversedLinkedListTest head){
+        ReversedLinkedListTest slow = head;
+        ReversedLinkedListTest fast = head;
+        while (fast != null && fast.listNode != null) {
+            slow = slow.listNode;
+            fast = fast.listNode.listNode;
+            if(slow == fast) return true;
+        }
+        return false;
     }
 }
