00e27e96331296bf413e082ae7924d22b99f1370
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-14 17:58:00 -0500

    Add BinaryTree.java

---
+++ src/main/java/solutions/trees/BinaryTree.java
@@ -0,0 +1,27 @@
+package solutions.trees;
+
+public class BinaryTree {
+    int val;
+    BinaryTree rightNode;
+    BinaryTree leftNode;
+    public BinaryTree(){}
+    public BinaryTree(int val) {this.val = val;}
+    public BinaryTree(int val, BinaryTree right, BinaryTree left){
+        this.val = val;
+        this.rightNode = right;
+        this.leftNode = left;
+    }
+
+    public static BinaryTree invertTree (BinaryTree root) {
+        if (root == null){
+            return null;
+        }
+        BinaryTree temp = root.leftNode;
+        root.leftNode = root.rightNode;
+        root.rightNode = temp;
+
+        invertTree(root.leftNode);
+        invertTree(root.rightNode);
+        return root;
+    }
+}
