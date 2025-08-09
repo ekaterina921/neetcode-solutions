46c6d1ba3cd57fa5ecfe252513e18e81e6ca4b5c
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-08-09 17:09:23 -0500

    Continue exploring Streams

--- src/main/java/solutions/arrays/DuplicatesInArrays.java
+++ src/main/java/solutions/arrays/DuplicatesInArrays.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DuplicatesInArrays {
+
+    //check if array has duplicates
     public boolean hasDuplicate(int[] nums) {
         Set<Integer> numsWithoutDuplicates = Arrays.stream(nums) // Create an IntStream
                 .boxed()          // Box each int to an Integer
@@ -14,6 +16,7 @@ public class DuplicatesInArrays {
         return nums.length != numsWithoutDuplicates.size();
     }
 
+    //check if array has duplicates
     public boolean hasDuplicateAltSolution(int[] nums){
         HashSet<Integer> numsWithoutDuplicates = new HashSet<>();
         for (int num : nums) {
@@ -25,13 +28,54 @@ public class DuplicatesInArrays {
         return false;
     }
 
+    //remove duplicates
     public static int[] distinct(int[] array){
         return Arrays.stream(array).distinct().toArray();
     }
-
+    //remove duplicates
     public static ArrayList<Integer> distinct1(ArrayList<Integer> arrayList){
         HashSet<Integer> hasSet = new HashSet<>(arrayList);
         return new ArrayList<>(hasSet);
+    }
+
+    public static <T> boolean checkForDuplicateValuesInArrays(ArrayList<T> arrayList1, ArrayList<T> arrayList2){
+        //brute force
+        for (T value : arrayList1) {
+            for (T t : arrayList2) {
+                if (value.equals(t)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static <T> boolean checkForDuplicateValuesInArrays1(ArrayList<T> arrayList1, ArrayList<T> arrayList2){
+        //using hashset (better time complexity)
+
+        if (arrayList1 == null || arrayList2 == null){
+            return false;
+        }
+
+        Set<T> newSet = new HashSet<>(arrayList1);
+        for (T value : arrayList2){
+            if (newSet.contains(value)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T> boolean checkForDuplicateValuesInArrays2(ArrayList<T> arrayList1, ArrayList<T> arrayList2){
+        //using streams
+        if (arrayList1 == null || arrayList2 == null){
+            return false;
+        }
+
+        HashSet<T> set1 = new HashSet<>(arrayList1);
+
+        return arrayList2.stream().anyMatch(set1::contains);
+
     }
 }
 
